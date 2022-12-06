5499c56239fa42d97cf3df133aa19396aec6336a
iipmango <>
2022-12-06 14:51:29 +0900

    tts에 날짜 삭제, 메뉴만 음성나오도록 조정

--- src/tts.py
+++ src/tts.py
@@ -1,18 +1,21 @@
 from gtts import gTTS
 import os
 import dbcon
+import datetime
 
 def tts():
+    today = datetime.datetime.today().weekday()
     
-    #Date = []
-    #Lunch = []
-    #Date, Lunch = dbcon.dbGet()
+    Date = []
+    Lunch = []
+    Date, Lunch = dbcon.dbGet()
+    menu = Lunch[today].split("\n")
+    text = ""
+    for i in range(4,10):
+        text += menu[i]
     
-    text = "heLLO"
-    tts = gTTS(text = text, lang = 'en')
+    tts = gTTS(text = text, lang = 'ko')
     tts.save("helloKR.mp3")
     
     os.system("mpg321 helloKR.mp3")
 
-
-tts()
